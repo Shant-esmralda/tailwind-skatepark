322a3966550edb1d734409af812cf4b602bf3d71
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-08-28 13:37:44 +0330

    button finished

--- src/button/primary-button.svelte
+++ src/button/primary-button.svelte
@@ -1,1 +1,3 @@
-<button class="rounded-md bg-blue-400 px-3 py-1.5 text-white shadow-md">Button</button>
+<button class="rounded-md bg-blue-400 px-3 py-1.5 text-white shadow-md hover:bg-blue-300"
+  >Button</button
+>
