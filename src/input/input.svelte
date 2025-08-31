698cb318163b57991242678bfdcf7daabbcd467b
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-08-31 12:33:58 +0330

    input

--- src/input/input.svelte
+++ src/input/input.svelte
@@ -4,9 +4,10 @@
   const errorId = error ? `${id}-error` : undefined;
 </script>
 
-<div>
-  <label for={id}>{label}</label>
+<div class="space-y-1">
+  <label class="block font-medium text-slate-900" for={id}>{label}</label>
   <input
+    class="block outline outline-slate-800 rounded px-2 py-1 focus:outline-2 focus:outline-blue-500 placeholder:italic placeholder-gray-400"
     {id}
     aria-describedby={descriptionId}
     aria-invalid={!!error}
