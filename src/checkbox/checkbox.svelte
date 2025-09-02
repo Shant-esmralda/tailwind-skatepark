6a42bbd66dc474fcf2c04c19c51f77466aee54fd
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-02 14:58:12 +0330

    checkbox group

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -2,7 +2,9 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div class="rounded border bg-slate-200 p-4 outline-offset-2 outline-sky-400 focus-within:bg-sky-200">
+<div
+  class="rounded border group bg-slate-200 p-4 outline-offset-2 outline-sky-400 focus-within:bg-sky-200"
+>
   <input class="peer accent-purple-300" {id} {...props} type="checkbox" />
-  <label class="peer-checked:line-through" for={id}>{label}</label>
+  <label class="peer-checked:line-through group-hover:font-bold" for={id}>{label}</label>
 </div>
