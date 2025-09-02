e57af9d57b4cf740e13d913c2542588a1d36ba4f
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-02 14:53:06 +0330

    checkbox

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -2,7 +2,7 @@
   const { label = 'A Very Important Checkbox', id = 'checkbox', ...props } = $props();
 </script>
 
-<div>
-  <input {id} {...props} type="checkbox" />
-  <label for={id}>{label}</label>
+<div class="rounded border bg-slate-200 p-4 outline-offset-2 outline-sky-400 focus-within:bg-sky-200">
+  <input class="peer accent-purple-300" {id} {...props} type="checkbox" />
+  <label class="peer-checked:line-through" for={id}>{label}</label>
 </div>
