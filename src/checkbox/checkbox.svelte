04bd374c4ff877a01f39062d32b3e6138efe2a8a
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-02 15:21:47 +0330

    transition

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -4,7 +4,7 @@
 
 <div
   class="group rounded border bg-slate-200 p-4 outline-offset-2 outline-sky-400 
-  has-checked:border-green-500 has-checked:bg-green-300"
+  has-checked:border-green-500 has-checked:bg-green-300 transition-colors"
 >
   <input type="checkbox" class="peer accent-green-700" {id} {...props}  />
   <label class="group-hover:font-bold peer-checked:line-through peer-checked:text-white" for={id}>{label}</label>
