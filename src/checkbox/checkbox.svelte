a37d6efdc4734c9e569fa795446256414b84f2de
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-03 12:48:38 +0330

    has checked advanced

--- src/checkbox/checkbox.svelte
+++ src/checkbox/checkbox.svelte
@@ -3,9 +3,21 @@
 </script>
 
 <div
-  class="group rounded border bg-slate-200 p-4 outline-offset-2 outline-sky-400 
-  has-checked:border-green-500 has-checked:bg-green-300 transition-colors"
+  class="group rounded border flex flex-col gap-2 bg-slate-200 p-4 outline-offset-2
+  outline-sky-400 transition-colors has-checked:border-green-500 has-checked:bg-green-300"
 >
-  <input type="checkbox" class="peer accent-green-700" {id} {...props}  />
-  <label class="group-hover:font-bold peer-checked:line-through peer-checked:text-white" for={id}>{label}</label>
+<div>
+  <input type="checkbox" class="peer accent-green-700" {id} {...props} />
+  <!-- can do it with group-has-checked:line-through instead of peer -->
+  <label class="group-hover:font-bold peer-checked:text-white peer-checked:line-through" for={id}
+    >{label}</label
+  >
+</div>
+  <div class=" hidden group-has-checked:block">
+    <label>What is the reason?</label>
+    <input
+      class="block rounded px-2 py-1 placeholder-gray-400 outline outline-slate-800 placeholder:italic focus:outline-2 focus:outline-blue-500"
+      placeholder="Enter the reason."
+    />
+  </div>
 </div>
