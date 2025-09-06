4a09a40f09e4930c17594612a9ee68f0e462ac65
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-06 14:56:58 +0330

    created new playground

---
+++ src/shant/shant.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Shant from './shant.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Shant',
+    component: Shant,
+  });
+</script>
+
+<Story name="Default" />
