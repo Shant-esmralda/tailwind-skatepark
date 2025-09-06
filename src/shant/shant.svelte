1f348550357abb1463a7e530a9ed0b3ddda3a6ac
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-06 15:18:02 +0330

    file tree challenge

---
+++ src/shant/shant.svelte
@@ -0,0 +1,23 @@
+<div>Hello, Shant!</div>
+
+<section>
+  <div>
+    <input type="checkbox" class="peer" id="folder" />
+    <label for="folder">Folder</label>
+  
+  <ul class="ml-4 peer-checked:opacity-30 pointer-events-none">
+    <li>
+      <input type="checkbox" id="file1" />
+      <label for="file1">File</label>
+    </li>
+    <li>
+      <input type="checkbox" id="file2" />
+      <label for="file2">File</label>
+    </li>
+    <li>
+      <input type="checkbox" id="file3" />
+      <label for="file3">File</label>
+    </li>
+  </ul>
+  </div>
+</section>
