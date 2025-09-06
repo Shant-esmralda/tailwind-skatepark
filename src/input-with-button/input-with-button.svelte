ee4ec43a65801c40f432c8a9d3dbd89abcef45f2
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-06 15:31:24 +0330

    input with button

--- src/input-with-button/input-with-button.svelte
+++ src/input-with-button/input-with-button.svelte
@@ -1,5 +1,6 @@
-<form>
-  <label for="new-task">What do you need to do?</label>
-  <input type="text" id="new-task" placeholder="New task…" />
-  <button>Submit</button>
+<form class="flex rounded px-3 py-1.5 outline outline-slate-600 focus-within:outline-blue-400 has-invalid:outline-red-500">
+  <!-- sr-only hiddens the tag but it is still there for screen readers -->
+  <label for="new-task" class="sr-only">What do you need to do?</label>
+  <input type="email" required class="flex-1 outline-none" id="new-task" placeholder="New task…" />
+  <button class="rounded bg-purple-600 px-3 py-1.5 text-white">Submit</button>
 </form>
