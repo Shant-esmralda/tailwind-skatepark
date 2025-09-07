857fbbb013075d50d2cbba852c2abaa20923573d
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-07 12:36:17 +0330

    flexbox nav layout

--- src/navigation/navigation.svelte
+++ src/navigation/navigation.svelte
@@ -1,12 +1,12 @@
 {#snippet link(title)}
   <li>
-    <a href="#">{title}</a>
+    <a class="px-2 py-5 hover:bg-amber-300" href="#">{title}</a>
   </li>
 {/snippet}
 
-<nav>
-  <a href="/">Unicorn Corp</a>
-  <ul>
+<nav class="flex items-center justify-between">
+  <a class="bg-gray-900 px-2 py-5 text-white" href="/">Unicorn Corp</a>
+  <ul class="flex">
     {@render link('Home')}
     {@render link('About')}
     {@render link('Services')}
