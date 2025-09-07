0b7200ae102321cd6bbd2fc9c07a6b8d2f308440
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-07 11:05:58 +0330

    flexbox 1

--- src/card-list/card-list.svelte
+++ src/card-list/card-list.svelte
@@ -2,7 +2,7 @@
   import Card from '../card/card.svelte';
 </script>
 
-<div class="space-y-2">
+<div class="flex flex-col md:flex-row gap-3 space-y-2">
   <Card title="Card Title" description="This is a description of the card." />
   <Card title="Another Card" description="This is another description of a different card." />
   <Card title="Third Card" description="This is a description of the third card." />
