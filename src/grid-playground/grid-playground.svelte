91f962c0c3f7932bbf8ba3ccc13b7975f9d984ce
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-11 13:17:01 +0330

    grid challange

--- src/grid-playground/grid-playground.svelte
+++ src/grid-playground/grid-playground.svelte
@@ -1,3 +1,16 @@
+
+<div class="grid grid-cols-1 gap-3 lg:grid-cols-[1fr_3fr]">
+  <div class="flex items-center justify-center rounded-md bg-sky-300 p-4 lg:col-span-2">Header</div>
+  <div class="flex items-center justify-center rounded-md bg-pink-300 p-4 lg:row-span-4">Sidebar</div>
+  <div class="flex items-center justify-center rounded-md bg-cyan-300 p-4">Content</div>
+  <div class="flex items-center justify-center rounded-md bg-indigo-300 p-4">Content</div>
+  <div class="flex items-center justify-center rounded-md bg-emerald-300 p-4">Content</div>
+  <div class="flex items-center justify-center rounded-md bg-amber-300 p-4">Content</div>
+  <div class="flex items-center justify-center rounded-md bg-lime-300 p-4 lg:col-span-2">Footer</div>
+</div>
+
+<br>
+
 <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-[1fr_1fr_2fr]">
   <div class="flex items-center justify-center rounded-md bg-sky-300 p-4">1</div>
   <div class="flex items-center justify-center rounded-md bg-pink-300 p-4">2</div>
