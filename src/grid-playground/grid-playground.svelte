52366c48e40927f2c4809ae458f1b7eb5c1da95f
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-09-11 11:58:48 +0330

    tailwing grid

--- src/grid-playground/grid-playground.svelte
+++ src/grid-playground/grid-playground.svelte
@@ -1,4 +1,4 @@
-<div class="space-y-4">
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-[1fr_1fr_2fr]">
   <div class="flex items-center justify-center rounded-md bg-sky-300 p-4">1</div>
   <div class="flex items-center justify-center rounded-md bg-pink-300 p-4">2</div>
   <div class="flex items-center justify-center rounded-md bg-cyan-300 p-4">3</div>
@@ -6,6 +6,6 @@
   <div class="flex items-center justify-center rounded-md bg-emerald-300 p-4">5</div>
   <div class="flex items-center justify-center rounded-md bg-amber-300 p-4">6</div>
   <div class="flex items-center justify-center rounded-md bg-rose-300 p-4">7</div>
-  <div class="flex items-center justify-center rounded-md bg-violet-300 p-4">8</div>
-  <div class="flex items-center justify-center rounded-md bg-lime-300 p-4">9</div>
+  <div class="flex items-center justify-center rounded-md bg-violet-300 p-4 lg:col-span-2">8</div>
+  <div class="flex items-center justify-center rounded-md bg-lime-300 p-4 lg:col-span-3">9</div>
 </div>
