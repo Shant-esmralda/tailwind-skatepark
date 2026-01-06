4b4f8d1bdf469630742a084f8e2e8b0388001803
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2026-01-06 12:52:05 +0330

    Update card.svelte

--- src/card/card.svelte
+++ src/card/card.svelte
@@ -2,7 +2,7 @@
   const { title = 'Card Title', description = '' } = $props();
 </script>
 
-<div class="border-blue-300 border-2 rounded-sm p-4 bg-blue-50">
+<div class="border-blue-300 border-2 rounded-sm p-4 bg-blue-50 dark:bg-slate-800 dark:border-slate-600">
   <h2 class="text-4xl font-bold">{title}</h2>
   <p class="mt-4 font-serif">{description}</p>
 </div>
